f7a351e165c9aaa63c52e37c8314050ce94d0b34
joselovi <39806815+joselovi@users.noreply.github.com>
2018-11-26 09:31:38 -0500

    Add files via upload

---
+++ src/app/mock-aves.ts
@@ -0,0 +1,14 @@
+import { Ave } from "./ave";
+
+export const AVES: Ave[] = [
+  { id: 1, nombreComun: 'Ave1', nombreCientifico: 'AveC1' },
+  { id: 2, nombreComun: 'Ave2', nombreCientifico: 'AveC1' },
+  { id: 3, nombreComun: 'Ave3', nombreCientifico: 'AveC1' },
+  { id: 4, nombreComun: 'Ave4', nombreCientifico: 'AveC1' },
+  { id: 5, nombreComun: 'Ave5', nombreCientifico: 'AveC1' },
+  { id: 6, nombreComun: 'Ave6', nombreCientifico: 'AveC1' },
+  { id: 7, nombreComun: 'Ave7', nombreCientifico: 'AveC1' },
+  { id: 8, nombreComun: 'Ave8', nombreCientifico: 'AveC1' },
+  { id: 9, nombreComun: 'Ave9', nombreCientifico: 'AveC1' },
+  { id: 10, nombreComun: 'Ave10', nombreCientifico: 'AveC1' }
+];
